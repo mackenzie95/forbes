356e94073f7e936458d5f33d2bd4c302b8674ca0
mackenzie95 <simeonobatu856@gmail.com>
2021-08-16 01:21:31 +0100

    second commit

--- phishing/settings.py
+++ phishing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*7yx&7i(d%+xx)=*&u8=-7mi7kvd2=^r(dt*uxtcq2w%3fpamj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Default primary key field type
