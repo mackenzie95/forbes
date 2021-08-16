bdab260af91657bdc0fc3ff8dacffbdaa643f0e7
mackenzie95 <simeonobatu856@gmail.com>
2021-08-16 19:51:06 +0100

    nineth commit

--- phishing/settings.py
+++ phishing/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-*7yx&7i(d%+xx)=*&u8=-7mi7kvd2=^r(dt*uxtcq2w%3fpamj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-        host=forbe.herokuapp.com
-        ]
+ALLOWED_HOSTS = ["forbe.herokuapp.com"]
 
 
 # Application definition
