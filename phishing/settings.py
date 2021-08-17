08f0b1f35bb8294ac59744bdd2b1f4a5f5815eb3
mackenzie95 <simeonobatu856@gmail.com>
2021-08-17 06:39:21 +0100

    first commit

--- phishing/settings.py
+++ phishing/settings.py
@@ -10,18 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
+from pathlib import path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-*7yx&7i(d%+xx)=*&u8=-7mi7kvd2=^r(dt*uxtcq2w%3fpamj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -122,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ('static'),
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
